483c6b251ef8be4ca1234c73e13cdf787e5ddad7
Jordan Walsh <jordanwalsh@gmail.com>
2019-08-25 02:10:40 -0400

    messing with tv shows

--- eztv.py
+++ eztv.py
@@ -105,7 +105,8 @@ def enqueue_transmission():
     download_candidates = shows_collection.find(
         {"parsed": True}).sort([("seeds", -1)])
     for candidate in download_candidates:
-        print candidate['show_title']
+        download_dir = BASE_DOWNLOAD_PATH + "/{}/Season {}/{} S{}E{}.mkv".format(candidate['show_title'], candidate['season'], candidate['show_title'], candidate['season'], candidate['episode')
+        print download_dir
 
 
 if __name__ == "__main__":
