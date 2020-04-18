803f11de738206b089136b25c3f10d602b31a2f7
Jordan Walsh <jowalsh@redhat.com>
2020-04-18 14:42:20 -0400

    fixed bad download

--- fix_downloaded.py
+++ fix_downloaded.py
@@ -3,18 +3,14 @@ from pymongo import MongoClient
 import os
 
 
-MONGO_HOST = "localhost"
+MONGO_HOST = "deluge.lan"
 MONGO_PORT = 27017
 MONGO_DB = 'yts'
-DIR = "/synology/jordan/Plex/Movies/YIFFY"
 
 mongo = MongoClient(MONGO_HOST, MONGO_PORT)
 yts_db = mongo['yts']
 movies_collection = yts_db['movies']
 
-for dir in os.listdir(DIR):
-   movie_check = dir.split("(")[0].strip()
-   matched_movie = movies_collection.find_one({"title":movie_check})
-   if matched_movie:
-       print "Marked {} as downloaded".format(matched_movie['title'].encode("utf-8"))
-       movies_collection.update_one({"_id":matched_movie['_id']}, {'$set':{"downloaded":True}})
+movies = movies_collection.find().limit(250).sort([("_id",-1)])
+for movie in movies:
+    movies_collection.update_one({"_id": movie['_id']},{"$set":{"downloaded":False}})
